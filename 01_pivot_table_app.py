101d615c36929301fcfe224fa5da441ce54f62a9
ferjen <ferjen.coder@gmail.com>
2024-02-11 19:32:08 -0300

    upd: created 00_one_report.py as exe

--- 01_pivot_table_app.py
+++ 01_pivot_table_app.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 
-df = pd.read_excel("Tablero DB_240210.xlsx")
+df = pd.read_excel("db/Tablero DB_240210.xlsx")
 
 df = df[["Sigla Esp", "Tipo HH", "Horas"]]
 # print(df)
@@ -13,4 +13,4 @@ pivot_table = df.pivot_table(
 )
 
 # Workbook name, tab name, start row (row where the pivot is going to start)
-pivot_table.to_excel("pivot_table.xlsx", "Report", startrow=4)
+pivot_table.to_excel("reports/pivot_table.xlsx", "Report", startrow=4)
--- 02_add_barchart_app.py
+++ 02_add_barchart_app.py
@@ -1,9 +1,10 @@
 from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 
-wb = load_workbook("pivot_table.xlsx")
+wb = load_workbook("reports/pivot_table.xlsx")
 sh = wb["Report"]
 
+# Use direct references from `sh` for min and max rows/columns
 min_column = wb.active.min_column
 max_column = wb.active.max_column
 min_row = wb.active.min_row
@@ -26,4 +27,4 @@ sh.add_chart(barchart, "A10")
 
 barchart.title = "Horas por especialidad"
 barchart.style = 2
-wb.save("barchart.xlsx")
+wb.save("reports/barchart.xlsx")
